6765a8045d4df1b23c783f1fa5a75fa924e490b1
Vineet Choudhary <vineetchoudhary@live.in>
2019-08-13 17:20:18 +0530

    Added Carthage Demo

---
+++ AppBoxSDKCarthageDemo/AppBoxSDKCarthageDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppBoxSDKCarthageDemo
+//
+//  Created by Vineet Choudhary on 13/08/19.
+//  Copyright © 2019 Developer Insider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
